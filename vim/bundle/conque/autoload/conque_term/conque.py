77ea0e5798b29a159888e356b320e1b6a4402d7b
QueezyTheGreat <queezythegreat@gmail.com>
2012-10-25 02:14:18 +0200

    Conque terminal buffer name changes when title set from terminal (statusline is left alone)

--- vim/bundle/conque/autoload/conque_term/conque.py
+++ vim/bundle/conque/autoload/conque_term/conque.py
@@ -1026,7 +1026,8 @@ class Conque:
 
         if key == '0' or key == '2':
 
-            vim.command('setlocal statusline=' + re.escape(val))
+            #vim.command('setlocal statusline=' + re.escape(val))
+            vim.command('file ' + re.escape(val))
             try:
                 vim.command('set titlestring=' + re.escape(val))
             except:
